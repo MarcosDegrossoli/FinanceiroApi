f73d44887a433a2fd4903c6c0372efdcf15c5be7
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-09-09 22:12:54 -0300

    feat: Add dbcontext factory

---
+++ Financeiro.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Financeiro.Infrastructure.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<FinanceiroDbContext>
+    {
+        public FinanceiroDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../FinanceiroApi"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<FinanceiroDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseNpgsql(connectionString);
+
+            return new FinanceiroDbContext(builder.Options);
+        }
+    }
+}
