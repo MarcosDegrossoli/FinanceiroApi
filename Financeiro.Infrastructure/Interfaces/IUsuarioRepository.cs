0b9a8a4ea48a42f5f253c589eedcfbb8fdacd4aa
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-09-16 21:09:21 -0300

    feat: Criação de repository de usuário

---
+++ Financeiro.Infrastructure/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using Financeiro.Domain.Entidades;
+
+namespace Financeiro.Infrastructure.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<IEnumerable<Usuario>> GetAllAsync();
+        Task<Usuario> GetByIdAsync(Guid id);
+        Task AddAsync(Usuario usuario);
+        Task UpdateAsync(Usuario usuario);
+        Task DeleteAsync(Guid id);
+        Task<Usuario> GetByCpfAsync(string cpf);
+    }
+}
---
+++ Financeiro.Infrastructure/Repositories/UsuarioRepository.cs
@@ -0,0 +1,54 @@
+﻿using Financeiro.Domain.Entidades;
+using Financeiro.Infrastructure.Data;
+using Financeiro.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financeiro.Infrastructure.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly FinanceiroDbContext _context;
+
+        public UsuarioRepository(FinanceiroDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Usuario>> GetAllAsync()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        public async Task<Usuario> GetByIdAsync(Guid id)
+        {
+            return await _context.Usuarios.FindAsync(id);
+        }
+
+        public async Task AddAsync(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Usuario> GetByCpfAsync(string cpf)
+        {
+            return await _context.Usuarios.FirstOrDefaultAsync(u => u.Cpf == cpf);
+        }
+    }
+}
