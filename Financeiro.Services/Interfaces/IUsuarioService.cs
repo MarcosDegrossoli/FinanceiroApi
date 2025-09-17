1c8cbd678ef93c99c9b3357fc5426af4c6f9b170
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-09-16 21:10:34 -0300

    feat: criação de service de usuário

---
+++ Financeiro.Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,13 @@
+﻿using Financeiro.Domain.Entidades;
+
+namespace Financeiro.Services.Interfaces
+{
+    public interface IUsuarioService
+    {
+        Task<IEnumerable<Usuario>> GetAllAsync();
+        Task<Usuario> GetByIdAsync(Guid id);
+        Task AddAsync(Usuario usuario);
+        Task UpdateAsync(Usuario usuario);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ Financeiro.Services/Services/UsuarioService .cs
@@ -0,0 +1,48 @@
+﻿using Financeiro.Domain.Entidades;
+using Financeiro.Infrastructure.Interfaces;
+using Financeiro.Services.Interfaces;
+
+namespace Financeiro.Services.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _repository;
+
+        public UsuarioService(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Usuario>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Usuario> GetByIdAsync(Guid id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(Usuario usuario)
+        {
+            var usuarioExistente = await _repository.GetByCpfAsync(usuario.Cpf);
+
+            if (usuarioExistente != null)
+            {
+                throw new Exception("Usuário com este CPF já existe.");
+            }
+
+            await _repository.AddAsync(usuario);
+        }
+
+        public async Task UpdateAsync(Usuario usuario)
+        {
+            await _repository.UpdateAsync(usuario);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+    }
+}
