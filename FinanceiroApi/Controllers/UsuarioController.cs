a52764ae7de1701e17587f9692daffecaf5e1eb3
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-09-16 21:10:58 -0300

    feat: Criação de controller de usuário

---
+++ FinanceiroApi/Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+﻿using Financeiro.Domain.Entidades;
+using Financeiro.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceiroApi.Application.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var usuarios = await _usuarioService.GetAllAsync();
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var usuario = await _usuarioService.GetByIdAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Usuario usuario)
+        {
+            await _usuarioService.AddAsync(usuario);
+            return CreatedAtAction(nameof(GetById), new { id = usuario.Id }, usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return BadRequest();
+            }
+            await _usuarioService.UpdateAsync(usuario);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _usuarioService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
