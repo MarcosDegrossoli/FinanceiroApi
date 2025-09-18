4296fe90e0fade6fade0a691ed3d931553d955bc
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-09-18 19:57:11 -0300

    feat: Add banco coltroller

---
+++ FinanceiroApi/Controllers/BancoController.cs
@@ -0,0 +1,24 @@
+﻿using Financeiro.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceiroApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BancoController : ControllerBase
+    {
+        private readonly IBancoService _bancoService;
+
+        public BancoController(IBancoService bancoService)
+        {
+            _bancoService = bancoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var bancos = await _bancoService.GetAllAsync();
+            return Ok(bancos);
+        }
+    }
+}
