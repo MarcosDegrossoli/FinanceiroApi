a639f759c93bde3c69eb9ef11fc662b28d4956b5
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-09-19 01:15:16 -0300

    feat: Add controller de contas

---
+++ FinanceiroApi/Controllers/ContasController.cs
@@ -0,0 +1,39 @@
+﻿using Financeiro.Domain.Entidades;
+using Financeiro.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceiroApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContasController : ControllerBase
+    {
+        private readonly IContasService _contasService;
+
+        public ContasController(IContasService contasService)
+        {
+            _contasService = contasService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var contas = await _contasService.GetAllAsync();
+            return Ok(contas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Conta conta)
+        {
+            try
+            {
+                await _contasService.AddAsync(conta);
+                return CreatedAtAction(nameof(GetAll), new { id = conta.IdConta }, conta);
+            }
+            catch (Exception execao)
+            {
+                return BadRequest(execao.Message);
+            }
+        }
+    }
+}
