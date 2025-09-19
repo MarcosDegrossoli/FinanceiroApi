170279507740a0f9e8fdd0a0b4c670ced083cd1b
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-09-19 01:14:47 -0300

    feat: Add service de contas

---
+++ Financeiro.Services/Interfaces/IContasService.cs
@@ -0,0 +1,11 @@
+﻿using Financeiro.Domain.Entidades;
+
+namespace Financeiro.Services.Interfaces
+{
+    public interface IContasService
+    {
+        Task<IEnumerable<Conta>> GetAllAsync();
+        Task<Conta?> GetByIdAsync(int id);
+        Task AddAsync(Conta conta);
+    }
+}
---
+++ Financeiro.Services/Services/ContasService.cs
@@ -0,0 +1,58 @@
+﻿using Financeiro.Domain.Entidades;
+using Financeiro.Infrastructure.Interfaces;
+using Financeiro.Services.Interfaces;
+
+namespace Financeiro.Services.Services
+{
+    public class ContasService : IContasService
+    {
+        public readonly IContasRepository _contasRepository;
+        public readonly IBancoRepository _bancoRepository;
+        public ContasService(IContasRepository repository, IBancoRepository bancoRepository)
+        {
+            _contasRepository = repository;
+            _bancoRepository = bancoRepository;
+        }
+
+        public async Task<IEnumerable<Conta>> GetAllAsync()
+        {
+            return await _contasRepository.GetAllAsync();
+        }
+
+        public async Task<Conta?> GetByIdAsync(int id)
+        {
+            return await _contasRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(Conta conta)
+        {
+            Banco? banco = await VerificarSeBancoExiste(conta);
+
+            await VerificarDuplicidadeConta(conta, banco);
+
+            await _contasRepository.AddAsync(conta);
+        }
+
+        private async Task VerificarDuplicidadeConta(Conta conta, Banco? banco)
+        {
+            var contaExistente = await _contasRepository.GetByNumeroConta(conta.NumeroConta);
+
+            if (contaExistente != null && contaExistente.IdBanco == banco.IdBanco)
+            {
+                throw new Exception("Conta com este número já existe para este banco.");
+            }
+        }
+
+        private async Task<Banco?> VerificarSeBancoExiste(Conta conta)
+        {
+            var banco = await _bancoRepository.GetByIdAsync(conta.IdBanco);
+
+            if (banco == null)
+            {
+                throw new Exception("Banco não encontrado.");
+            }
+
+            return banco;
+        }
+    }
+}
