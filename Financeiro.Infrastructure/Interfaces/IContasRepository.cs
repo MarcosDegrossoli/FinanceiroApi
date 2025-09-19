8f1a43d8a81af5111bcfb3f25562b15ffafab8c4
Marcos Eduardo Degrossoli <98660927+MarcosDegrossoli@users.noreply.github.com>
2025-09-19 01:14:26 -0300

    feat: Add repositorio contas

---
+++ Financeiro.Infrastructure/Interfaces/IContasRepository.cs
@@ -0,0 +1,12 @@
+﻿using Financeiro.Domain.Entidades;
+
+namespace Financeiro.Infrastructure.Interfaces
+{
+    public interface IContasRepository
+    {
+        Task<IEnumerable<Conta>> GetAllAsync();
+        Task<Conta?> GetByIdAsync(int id);
+        Task AddAsync(Conta conta);
+        Task<Conta> GetByNumeroConta(string numeroConta);
+    }
+}
---
+++ Financeiro.Infrastructure/Repositories/ContasRepository.cs
@@ -0,0 +1,38 @@
+﻿using Financeiro.Domain.Entidades;
+using Financeiro.Infrastructure.Data;
+using Financeiro.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financeiro.Infrastructure.Repositories
+{
+    public class ContasRepository : IContasRepository
+    {
+        private readonly FinanceiroDbContext _context;
+
+        public ContasRepository(FinanceiroDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Conta>> GetAllAsync()
+        {
+            return await _context.Contas.ToListAsync();
+        }
+
+        public async Task<Conta?> GetByIdAsync(int id)
+        {
+            return await _context.Contas.FindAsync(id);
+        }
+
+        public async Task AddAsync(Conta conta)
+        {
+            _context.Contas.Add(conta);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Conta> GetByNumeroConta(string numeroConta)
+        {
+            return await _context.Contas.FirstOrDefaultAsync(c => c.NumeroConta == numeroConta);
+        }
+    }
+}
